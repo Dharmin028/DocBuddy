b82ca6fcc55a89fc85bd710df7602be960b80709
Dharmin <dharminrajkotiya2005@gmail.com>
2025-08-24 15:45:53 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="docbuddy",
+    version="0.0.0",
+    author="Dharmin Rajkotiya",
+    author_email="dharminrajkotiya2005@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
